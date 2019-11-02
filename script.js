52e85837fa776d67351d4b6d138117a8c00899dd
rhryhoryk <eh480.35@gmail.com>
2019-11-03 01:40:29 +0300

    feat: render DOM

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 'use strict';
 (function () {
 
+  // ---------------------------creating DOM via fragment-----------------------------------------
+  var fragment = document.createDocumentFragment();
+
+  var form = document.createElement('form');
+  var textarea = document.createElement('textarea');
+
+  form.appendChild(textarea);
+  fragment.appendChild(form);
+
+  var keyboard = document.createElement('div');
+  keyboard.classList.add('keyboard');
+  for (var i = 0; i < window.keysBD.keys.length; i++) {
+    var button = document.createElement('button');
+    var keyCod = window.keysBD.keys[i][0];
+    button.classList.add('button');
+    if (keyCod === 27 || keyCod === 9 || keyCod === 20 || keyCod === 17 || keyCod === 18 || keyCod === 46) {
+      button.classList.add('--double');
+    }
+    if (keyCod === 13) {
+      button.classList.add('--enter');
+    }
+    if (keyCod === 16) {
+      button.classList.add('--shift');
+    }
+    if (keyCod === 32) {
+      button.classList.add('--space');
+    }
+    button.innerHTML = window.keysBD.keys[i][1];
+    keyboard.appendChild(button);
+  }
+  fragment.appendChild(keyboard);
+
+  var renderFragment = function (frg) {
+    var body = document.querySelector('body');
+    body.appendChild(frg);
+  };
+
+  window.addEventListener('load', function () {
+    renderFragment(fragment);
+  });
 })();
